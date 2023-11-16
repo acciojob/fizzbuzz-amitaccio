82f55f1d7d562fde8312a0fdae1e42c767be74e6
amitaccio <142372667+amitaccio@users.noreply.github.com>
2023-11-16 18:31:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+// multiple of any number means the when we divide that number 
+// respective multiple the remainder will be zero.
+
+for (let i=1; i<101; i++){
+	if(i % 3 == 0){
+		alert("Fizz");
+	}
+	else if(i % 5 == 0){
+		 alert("Buzz");
+	}
+	else if(i % 15 == 0){
+		 alert("FizzBuzz");
+	}
+	else{
+	    alert(i);
+	}
+}
